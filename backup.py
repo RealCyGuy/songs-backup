ee61fcfa4d4989e60b753747cb262736e16e40ce
Cyrus Yip <cyruscmyip1@gmail.com>
2022-09-13 17:35:17 -0700

    backup?

---
+++ backup.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+from dotenv import load_dotenv
+from pyyoutube import Api
+
+load_dotenv()
+api = Api(api_key=os.getenv("API_KEY"))
+data = api.get_playlist_items(
+    playlist_id="PLRct1-5In-8Ewg5Kq-0JP8wh3ZweOXH9A",
+    parts=["snippet", "contentDetails", "status"],
+    count=None,
+    return_json=True
+)
+for item in data["items"]:
+    with open(f"songs/{item['snippet']['resourceId']['videoId']}.json", "w") as file:
+        json.dump(item, file, indent=4)
